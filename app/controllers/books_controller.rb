b15863cda035b1218fcfaf984489742c1e36c963
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-06-04 15:12:08 +0000

    [add] Bookers2_user_edit_show_and_book_index_show_except_user_screen

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,30 @@
 class BooksController < ApplicationController
 
   def index
-    @book = Book.new
+    @post_book = Book.new
     @books = Book.all
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    @post_book = Book.new(book_params)
+    @post_book.user_id = current_user.id
+    @post_book.save
+    redirect_to book_path(@post_book.id)
   end
 
   def edit
 
   end
 
-
-
   def show
+    @post_book = Book.new
+    @book = Book.find(params[:id])
+  end
 
+  def destroy
+    post_book = Book.find(params[:id])
+    post_book.destroy
+    redirect_to books_path
   end
 
   private
